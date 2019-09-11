5814f77fc4853f9d30f22103bda7bee6deb76fff
hukish <hudsonhukish@gmail.com>
2019-09-11 10:27:57 +0300

    added role class

--- app/models.py
+++ app/models.py
@@ -20,3 +20,32 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(255))
     reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
     pass_secure = db.Column(db.String(255))
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
