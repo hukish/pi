3dddf1acc5d0c48056e55018391043b5de76b77b
hukish <hudsonhukish@gmail.com>
2019-09-11 10:46:36 +0300

    added email configurations

--- app/config.py
+++ app/config.py
@@ -1,3 +1,15 @@
 import os
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hudson:12345@localhost/PITCHES'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+     #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    def init_app(app):
+        pass
