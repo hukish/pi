f51257fcf87bc2c8a49e91000ba4c8fad5262ce2
hukish <hudsonhukish@gmail.com>
2019-09-11 10:25:59 +0300

    update changes

--- app/models.py
+++ app/models.py
@@ -7,3 +7,16 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    pass_secure = db.Column(db.String(255))
