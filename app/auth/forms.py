6746f1f06c7a9ac4ae068ba1a3b89d3e4d599b4f
hukish <hudsonhukish@gmail.com>
2019-09-11 11:16:39 +0300

    added a loginform

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
