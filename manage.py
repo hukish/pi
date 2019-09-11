00568a879027b5a9323f7e68cb8377d82173164a
hukish <hudsonhukish@gmail.com>
2019-09-11 10:41:59 +0300

    update changes

---
+++ manage.py
@@ -0,0 +1,19 @@
+import app
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Role,Review
+from  flask_migrate import Migrate, MigrateCommand
+from flask_wtf.csrf import CSRFProtect # add csrf protection without creating a FlaskForm
+from app import config
+
+import config
+import os
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
