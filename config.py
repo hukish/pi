7968c09999c69941653fb6225bf4bc2192941756
hukish <hudsonhukish@gmail.com>
2019-09-13 15:40:07 +0300

    deployment

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("oliveenterprise2@gmail.com")
+    MAIL_PASSWORD = os.environ.get("NOTEKCS158M")
 
 
 
